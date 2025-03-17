6c21fccc50271f539731a0ed16ea7d450b999e12
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-03-16 23:28:01 -0400

    Update mainMovement.cs

--- mainMovement.cs
+++ mainMovement.cs
@@ -5,36 +5,35 @@ using Debug = UnityEngine.Debug;
 
 public class mainMovemen : MonoBehaviour
 {
+    //initialize player
     [SerializeField]
     GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        //move up if w or up arrow is pressed
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
         {
             Vector3 increment = new Vector3(0, 0.01f, 0);
             player.transform.position = player.transform.position + increment;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        //move up if s or down arrow is pressed
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             Vector3 increment = new Vector3(0, -0.01f, 0);
             player.transform.position = player.transform.position + increment;
         }
 
-        if (Input.GetKey(KeyCode.A))
+        //move up if a or left arrow is pressed
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 increment = new Vector3(-0.01f, 0, 0);
             player.transform.position = player.transform.position + increment;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        //move up if d or right arrow is pressed
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             Vector3 increment = new Vector3(0.01f, 0, 0);
             player.transform.position = player.transform.position + increment;
