eaa0dfe17ad13b251df6c0123384a984e66c1b57
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-08 20:47:35 -0400

    Update Health.cs

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    public int maxHealth;
+    public int currentHealth;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    //amount is sent from enemy script
+    void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            //dead
+            //play death animation
+            //show game over screen?????
+        }
     }
+
+    void Heal(int gain)
+    {
+        currentHealth += gain;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
 }
