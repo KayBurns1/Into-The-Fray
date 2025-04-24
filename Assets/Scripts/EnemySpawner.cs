34330cad1107aac8b46cfd6944d6260eca0f52fb
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-23 22:17:48 -0400

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,16 +11,54 @@ public class EnemySpawner : MonoBehaviour
 
     //how long it takes for the enemy to spawn
     [SerializeField]
-    private float swordSwarmerInterval = 3.5f;
+    private float swordSwarmerInterval = 10f;
     [SerializeField]
-    private float rangedSwarmerInterval = 5f;
+    private float rangedSwarmerInterval = 15f;
+
+    //countdown timer to increase spawning interval
+    private float countdown = 30f;
+    private float count = 0;
+
+
+    //calling script so we can access the bool to start spawning
+    public BarFightScene bfs;
+    public bool fight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(swordSwarmerInterval, swordSwarmer));
-        StartCoroutine(spawnEnemy(rangedSwarmerInterval, rangedSwarmer));
+        //StartCoroutine(spawnEnemy(swordSwarmerInterval, swordSwarmer));
+        //StartCoroutine(spawnEnemy(rangedSwarmerInterval, rangedSwarmer));
+        swordSwarmerInterval = 10f;
+        rangedSwarmerInterval = 15f;
     }
+
+    private void Update()
+    {
+        if (bfs.dialogueCompleted)
+        {
+            bfs.dialogueCompleted = false;
+            fight = true;
+            //start spawning
+            StartCoroutine(spawnEnemy(swordSwarmerInterval, swordSwarmer));
+            StartCoroutine(spawnEnemy(rangedSwarmerInterval, rangedSwarmer));
+        }
+
+        while (fight)
+        {
+            count += Time.deltaTime;
+
+            if (count >= countdown)
+            {
+                swordSwarmerInterval -= .1f;
+                rangedSwarmerInterval -= .1f;
+                count = 0;
+            }
+        }
+
+    }
+
+    //create update function to start coroutine if dialogueCompleted == true
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
