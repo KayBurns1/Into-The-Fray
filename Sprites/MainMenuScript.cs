3a2fead353717744b49a45fb266b6ae3bd827a69
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-25 19:20:25 -0400

    Update MainMenuScript.cs
    added kill count to be 0 when the game starts

--- Sprites/MainMenuScript.cs
+++ Sprites/MainMenuScript.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    
+    public Button play;
+    public Button quit;
+    public EnemyHealth em;
+
     public void OnApplicationQuit()
     {
         Application.Quit();
@@ -12,11 +18,12 @@ public class MainMenuScript : MonoBehaviour
 
     public void Play()
     {
+        em.killCount = 0;
         SceneManager.LoadScene("SC Demo");
     }
 
     public void Settings()
-{
-    SceneManager.LoadScene("Settings");
-}
+    {
+        SceneManager.LoadScene("Settings");
+    }
 }
