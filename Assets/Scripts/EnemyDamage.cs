f85b2fc10c4f809fe323ce9e64c496d4057b787d
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-25 22:15:45 -0400

    Update EnemyDamage.cs

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -4,14 +4,62 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    //enemy objects
+    [SerializeField]
+    private GameObject swordSwarmer;
+    [SerializeField]
+    private GameObject rangedSwarmer;
+
+    //player target
+    public Transform target;
+
+    //how much damage can be done
     public int damage;
+    //reference to player health
     public Health playerHealth;
+
+    //reference to enemy movement
+    public EnemyMovement movement;
+
+    //how often range enemy shoots
+    public float fireRate;
+    //var to keep track of when the range enemy can shoot
+    public float timeToFire;
+
+    //idk i forgor
+    public Transform fireingPoint;
+
+    private void Update()
+    {
+        //shoot if enemy is rangedSwarmer
+        if (rangedSwarmer)
+        {
+            Shoot();
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        //collide and cause damage if swordSwarmer collides with player
+        if (swordSwarmer)
         {
-            playerHealth.TakeDamage(damage);
+            if (collision.gameObject.tag == "Player")
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        //shoot when time or else subtract
+        if (timeToFire <= 0f)
+        {
+            //shoot
+            timeToFire = fireRate;
+        } else
+        {
+            timeToFire -= Time.deltaTime;
         }
     }
 
