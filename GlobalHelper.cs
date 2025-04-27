27597a4d77064359eac2ffc58e52a6ad4b21c9b8
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-27 15:06:42 -0400

    Update GlobalHelper.cs
    added comments to code

--- GlobalHelper.cs
+++ GlobalHelper.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public static class GlobalHelper
 {
+    //function to create a unique id for any object
     public static string GenerateUniqueID(GameObject obj)
     {
+        //returns a unique id for the given object
         return $"{obj.scene.name}_{obj.transform.position.x}_{obj.transform.position.y}";
     }
 }
