bd76372792992f05b855886eba05d848d36d0fd1
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-26 22:32:00 -0400

    Update settingsMenu.cs

--- settingsMenu.cs
+++ settingsMenu.cs
@@ -1,10 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class settingsMenu : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
-    public void backToGame()
+
+    public AudioMixer mixer;
+    public Button menu;
+
+    public void SetVolume(float volume)
     {
-        SceneManager.LoadScene("PauseMenu");
+        mixer.SetFloat("volume", volume);
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
