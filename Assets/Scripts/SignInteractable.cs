c66578d48d8f2db562f3babd7fd017e26e3eb4c7
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-27 15:43:26 -0400

    Update SignInteractable.cs

--- Assets/Scripts/SignInteractable.cs
+++ Assets/Scripts/SignInteractable.cs
@@ -5,25 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class SignInteractable : MonoBehaviour
 {
-    //this is the script for the NPC that starts the fight dialogue scene
 
-    public bool Interacted { get; private set; }
-    public string SignID { get; private set; }
+    public bool Interacted { get; private set; } //set bool for if sign is interacted or not
+    public string SignID { get; private set; } //create var to keep track of sign id
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //declare sign id if it doesnt exist
         SignID ??= GlobalHelper.GenerateUniqueID(gameObject);
     }
 
     public bool CanInteract()
     {
+        //make sure it is not interacted at first
         return !Interacted;
     }
 
     public void Interact()
     {
+        //make sure it cant interact if it is not interacted yet
         if (!CanInteract())
         {
             return;
