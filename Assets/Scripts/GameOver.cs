9a571202d1a9f4ecc7a6435ad5f008b3af53cf41
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-27 15:17:33 -0400

    Update GameOver.cs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,30 +5,36 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
-{
-    public Button Restart;
-    public int highScore;
-    public int currentScore;
-    public Text highScoreDisplay;
-    public Text currentScoreDisplay;
+{   
+    public Button Restart; //button to take you back to the main menu
+    public int highScore; //var to keep track of high score
+    public Text highScoreDisplay; //text to show high score
+    public Text currentScoreDisplay; //text to show current score
+
+    //reference to scoring script
+    public Scoring score;
 
     public void Start()
     {
-        if (currentScore >= highScore)
+        //if current score is higher than high score
+        if (score.currentScore >= highScore)
         {
-            highScore = currentScore;
+            //set high score to be current score and display the two
+            highScore = score.currentScore;
             highScoreDisplay.text = "High Score : " + highScore.ToString();
-            currentScoreDisplay.text = "Current Score : " + currentScore.ToString();
-            currentScore = 0;
+            currentScoreDisplay.text = "Current Score : " + score.currentScore.ToString();
+            score.currentScore = 0; //reset the current score
         } else
         {
+            //display high score and current score
             highScoreDisplay.text = "High Score : " + highScore.ToString();
-            currentScoreDisplay.text = "Current Score : " + currentScore.ToString();
-            currentScore = 0;
+            currentScoreDisplay.text = "Current Score : " + score.currentScore.ToString();
+            score.currentScore = 0; //reset the current score
         }
     }
 
-    public void Menu()
+    //go back to the main menu
+    public void Restarting()
     {
         SceneManager.LoadScene("MainMenu");
     }
