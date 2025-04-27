b28d22db6633a27e523940c9f9aa057ad9dfb23c
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-27 15:43:40 -0400

    Update NPCFight.cs

--- NPCFight.cs
+++ NPCFight.cs
@@ -8,27 +8,31 @@ public class NPCFight : MonoBehaviour, IInteractable
 {
     //this is the script for the NPC that starts the fight dialogue scene
 
-    public bool Talked { get; private set; }
-    public string NPCID {  get; private set; }
+    public bool Talked { get; private set; } //set bool for if NPC was talked to yet or not
+    public string NPCID {  get; private set; } //create var to keep track of NPC id
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //declare NPC id if it doesnt exist
         NPCID ??= GlobalHelper.GenerateUniqueID(gameObject);
     }
 
     public bool CanInteract()
     {
+        //make sure it is not interacted at first
         return !Talked;
     }
 
     public void Interact()
     {
+        //make sure it cant interact if it is not interacted yet
         if (!CanInteract()){
             return;
         }
 
+        //if it can interact make sure to load the dialogue scene
         SceneManager.LoadScene("BarFight");
 
     }
