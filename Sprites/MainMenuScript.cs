b4f00be6f69d4d54958269cf7bc74447b9b84e7f
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-27 15:14:42 -0400

    Update MainMenuScript.cs

--- Sprites/MainMenuScript.cs
+++ Sprites/MainMenuScript.cs
@@ -6,24 +6,33 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    
+    //declaring all the buttons in the scene
     public Button play;
+    public Button settings;
     public Button quit;
-    public EnemyHealth em;
+    //reference to the score script
+    public Scoring score;
 
+    //quit application function
     public void OnApplicationQuit()
     {
+        //quit game
         Application.Quit();
     }
 
+    //play function
     public void Play()
     {
-        em.killCount = 0;
+        //update the current score to be 0
+        score.currentScore = 0;
+        //load main game scene
         SceneManager.LoadScene("SC Demo");
     }
 
+    //settings function
     public void Settings()
     {
+        //load settings scene
         SceneManager.LoadScene("Settings");
     }
 }
