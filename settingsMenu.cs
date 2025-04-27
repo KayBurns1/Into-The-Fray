12a9bbd256009f9a9288d5f3ff8e65eefe8bfbe8
k1t69 <168482966+k1t69@users.noreply.github.com>
2025-04-27 15:08:14 -0400

    Update settingsMenu.cs

--- settingsMenu.cs
+++ settingsMenu.cs
@@ -7,14 +7,18 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    
+    public AudioMixer mixer; //var for the mixer to increase/decrease volume
+    public Button menu; //menu button to go back
 
-    public AudioMixer mixer;
-    public Button menu;
-
+    //function to increase/decrease volume
     public void SetVolume(float volume)
     {
+        //attach the mixer to an actual value
         mixer.SetFloat("volume", volume);
     }
+
+    //function to go back to the main menu
     public void Menu()
     {
         SceneManager.LoadScene("MainMenu");
