6cb92c05cd52acf107ac9ac67d0c92487bcafd31
zexlier <owen34223@gmail.com>
2025-04-27 22:51:43 -0400

    Newly imported assets

--- Into the Frey/Assets/Scripts/settingsMenu.cs
+++ Into the Frey/Assets/Scripts/settingsMenu.cs
@@ -1,10 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class settingsMenu : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
-    public void backToGame()
+    
+    public AudioMixer mixer; //var for the mixer to increase/decrease volume
+    public Button menu; //menu button to go back
+    public AudioSource audio; //audio source to mute
+
+    //function to increase/decrease volume
+    public void SetVolume(float volume)
     {
-        SceneManager.LoadScene("PauseMenu");
+        //attach the mixer to an actual value
+        mixer.SetFloat("volume", volume);
+    }
+
+    //function to go back to the main menu
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Mute(bool mute)
+    {
+        if(mute)
+        {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            AudioListener.volume = 1;
+        }
     }
 }
