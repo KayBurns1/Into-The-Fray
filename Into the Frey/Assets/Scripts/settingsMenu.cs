9cefcf56b4abf609b5db6076560868b95ae8472a
zexlier <owen34223@gmail.com>
2025-04-28 20:19:39 -0400

    Pre-push for animating purposes

--- Into the Frey/Assets/Scripts/settingsMenu.cs
+++ Into the Frey/Assets/Scripts/settingsMenu.cs
@@ -7,33 +7,27 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    
     public AudioMixer mixer; //var for the mixer to increase/decrease volume
     public Button menu; //menu button to go back
-    public AudioSource audio; //audio source to mute
 
     //function to increase/decrease volume
     public void SetVolume(float volume)
     {
+        float dB;
+
+        if (volume == 0)
+        dB = -80f; // -80 dB is basically mute
+        else
+        dB = Mathf.Log10(volume) * 20f;
+
         //attach the mixer to an actual value
-        mixer.SetFloat("volume", volume);
+        mixer.SetFloat("volume", dB);
+        Debug.Log($"Slider Value: {volume} -> dB: {dB}");    
     }
 
     //function to go back to the main menu
     public void Menu()
     {
         SceneManager.LoadScene(0);
-    }
-
-    public void Mute(bool mute)
-    {
-        if(mute)
-        {
-            AudioListener.volume = -80;
-        }
-        else
-        {
-            AudioListener.volume = 0;
-        }
     }
 }
